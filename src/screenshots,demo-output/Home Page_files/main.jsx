3a2135e426233b5b968588ccd4d1664e25bcc458
viswanathans3005 <viswanathans3005@gmail.com>
2026-04-10 18:38:12 +0530

    add a website demo page

---
+++ src/screenshots,demo-output/Home Page_files/main.jsx
@@ -0,0 +1,19 @@
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=59287c5b"; const StrictMode = __vite__cjsImport0_react["StrictMode"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=59287c5b"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.jsx";
+import __vite__cjsImport3_aos from "/node_modules/.vite/deps/aos.js?v=59287c5b"; const AOS = __vite__cjsImport3_aos;
+import "/node_modules/aos/dist/aos.css";
+var _jsxFileName = "C:/Users/Visu S/Documents/my projects/New Pro/Musule Up/src/main.jsx";
+import __vite__cjsImport5_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=59287c5b"; const _jsxDEV = __vite__cjsImport5_react_jsxDevRuntime["jsxDEV"];
+AOS.init();
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(StrictMode, { children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 11,
+	columnNumber: 5
+}, this) }, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 10,
+	columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJtYXBwaW5ncyI6IkFBQUEsU0FBUyxrQkFBa0I7QUFDM0IsU0FBUyxrQkFBa0I7QUFFM0IsT0FBTyxTQUFTO0FBQ2hCLE9BQU8sU0FBUztBQUNoQixPQUFPOzs7QUFFUCxJQUFJLE1BQU07QUFDVixXQUFXLFNBQVMsZUFBZSxPQUFPLENBQUMsQ0FBQyxPQUMxQyx3QkFBQyxZQUFELFlBQ0Usd0JBQUMsS0FBRCxFQUFPOzs7O1VBQ0k7Ozs7U0FDZCIsIm5hbWVzIjpbXSwic291cmNlcyI6WyJtYWluLmpzeCJdLCJ2ZXJzaW9uIjozLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBTdHJpY3RNb2RlIH0gZnJvbSAncmVhY3QnXG5pbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcblxuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC5qc3gnXG5pbXBvcnQgQU9TIGZyb20gXCJhb3NcIjtcbmltcG9ydCBcImFvcy9kaXN0L2Fvcy5jc3NcIjtcblxuQU9TLmluaXQoKTtcbmNyZWF0ZVJvb3QoZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3Jvb3QnKSkucmVuZGVyKFxuICA8U3RyaWN0TW9kZT5cbiAgICA8QXBwIC8+XG4gIDwvU3RyaWN0TW9kZT4sXG4pXG4iXX0=
